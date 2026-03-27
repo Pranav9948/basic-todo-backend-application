e4db4e4bba3edef80f86583337fd951350019fb8
Pranav9948 <pranavat9948@gmail.com>
2026-03-28 00:57:39 +0530

    added

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   password: '2026',
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 5432,
   database: 'todo_app',
 });
